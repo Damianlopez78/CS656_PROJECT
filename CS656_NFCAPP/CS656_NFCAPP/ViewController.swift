5bc6d4384a06d2322e4e13b437b5a7026d5544d8
Damian Lopez <Damianlopez_78@hotmail.com>
2019-02-16 13:15:23 -0500

    as practice I created an interface that takes user input and displays in a text box

--- CS656_NFCAPP/CS656_NFCAPP/ViewController.swift
+++ CS656_NFCAPP/CS656_NFCAPP/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //create mesg variable
+    var msg: String = ""
 
+    @IBOutlet weak var txt_msg_view: UITextView!
+    @IBOutlet weak var txt_msg_enter: UITextField!
+    @IBAction func sndMsg(_ sender: Any) {
+        //getting the input msg
+        msg = txt_msg_enter.text!
+        //displaying the msgs
+        txt_msg_view.text = msg
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
